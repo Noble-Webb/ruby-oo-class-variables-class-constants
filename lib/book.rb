2a9e914a520ccc97de9a44b8a28386e7710c52b1
Justin_Webb <Webb.n.justin@gmail.com>
2020-09-20 14:06:27 -0600

    finished lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,13 +1,22 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count #remove genre from attr_accessor 
+  attr_reader :title, :genre #add genre to the attr_reader
 
-  def initialize(title)
+  GENRES = [] #Class constant that holds all genres
+
+  def initialize(title) #does not initialize with a genre
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
+  #create the writer for genre and add the logicfor the class constant
+  def genre=(genre) 
+    @genre = genre
+    GENRES << genre
+  end
+
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,11 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS = [] #Class constant that will contain brands
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand) #shovels all unique instances of brand into the class constant BRANDS 
   end
 
   def cobble
